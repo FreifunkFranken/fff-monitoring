328b74a48dd8a77c3fac968daedc4e65f7d969fd
Dominik Heidler <dominik@heidler.eu>
2015-09-02 11:26:39 +0200

    add linebreaks

--- map/mkcsv.py
+++ map/mkcsv.py
@@ -8,16 +8,29 @@ routers = db.routers
 
 with open("csv/routers.csv", "w") as csv:
 	csv.write("lng,lat,status\n")
-	for router in routers.find({"position.lat": {"$exists": True}, "position.lng": {"$exists": True}}):
-		csv.write("%f,%f,%s\n" % (router["position"]["lng"], router["position"]["lat"], router.get("status", "unknown")))
+	for router in routers.find({
+		"position.lat": {"$exists": True},
+		"position.lng": {"$exists": True}}):
+		csv.write("%f,%f,%s\n" % (
+			router["position"]["lng"],
+			router["position"]["lat"],
+			router.get("status", "unknown")
+		))
 
 with open("csv/links.csv", "w") as csv:
 	csv.write("WKT,quality\n")
-	for router in routers.find({"position.lat": {"$exists": True}, "position.lng": {"$exists": True}, "neighbours": {"$exists": True}}):
+	for router in routers.find({
+		"position.lat": {"$exists": True},
+		"position.lng": {"$exists": True},
+		"neighbours": {"$exists": True}}):
 		for neighbour in router["neighbours"]:
 			if not "_id" in neighbour:
 				continue
-			neighbour_router = routers.find_one({"_id": neighbour["_id"], "position.lat": {"$exists": True}, "position.lng": {"$exists": True}})
+			neighbour_router = routers.find_one({
+				"_id": neighbour["_id"],
+				"position.lat": {"$exists": True},
+				"position.lng": {"$exists": True}
+			})
 			if neighbour_router:
 				csv.write("\"LINESTRING (%f %f,%f %f)\",%i\n" % (
 					router["position"]["lng"],
