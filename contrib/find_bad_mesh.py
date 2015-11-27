e93c17042405a1e5ca38a0fcf8aef042701a64fa
Dominik Heidler <dominik@heidler.eu>
2015-11-27 15:54:47 +0100

    update find_bad_mesh script

--- contrib/find_bad_mesh.py
+++ contrib/find_bad_mesh.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from pymongo import MongoClient
 client = MongoClient()
 
 db = client.freifunk
 
-for router in db.routers.find({"hood": {"$exists": True}, "neighbours": {"$exists": True}}):
+for router in db.routers.find({"hood": {"$exists": True}, "neighbours": {"$exists": True}, "status": "online"}, {"stats": 0}):
 	for neighbour in router["neighbours"]:
 		if "_id" in neighbour and "position" in neighbour:
-			neighbour_router = db.routers.find_one({"_id": neighbour["_id"]})
-			if router["hood"] != neighbour_router["hood"]:
+			neighbour_router = db.routers.find_one({"_id": neighbour["_id"]}, {"stats": 0})
+			if router["hood"] != neighbour_router["hood"] and neighbour_router["status"] == "online":
 				print("Illegal inter-hood-mesh between %s (%s) and %s (%s)!" % (router["hostname"], router["hood"], neighbour_router["hostname"], neighbour_router["hood"]))
