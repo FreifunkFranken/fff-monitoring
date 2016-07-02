9ec37e715b7adfb3c0110575f13443cdf58d4739
Steffen Pankratz <kratz00@gmx.de>
2016-07-02 18:57:33 +0200

    Added hood Aschaffenburg
    Signed-off-by: Steffen Pankratz <kratz00@gmx.de>

--- ffmap/db/hoods.py
+++ ffmap/db/hoods.py
@@ -73,6 +73,12 @@ hoods = [
         "name": "hof",
         "net": "10.50.104.0/21",
         "position": {"type": "Point", "coordinates": [12.000519, 50.127736]}
+},
+{
+	"keyxchange_id": 13,
+	"name": "aschaffenburg",
+	"net": "10.50.96.0/22",
+	"position": {"type": "Point", "coordinates": [9.886394, 49.986113]}
 }]
 
 for hood in hoods:
