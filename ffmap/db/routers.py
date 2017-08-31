f51d0ae5fdc81bc1885b11abc5175a91a24f7160
Dominik Heidler <dominik@heidler.eu>
2017-08-31 13:16:56 +0200

    Add index for "created" routerdb field (fixes #65)

--- ffmap/db/routers.py
+++ ffmap/db/routers.py
@@ -8,6 +8,7 @@ db = client.freifunk
 # create db indexes
 db.routers.create_index("hostname")
 db.routers.create_index("status")
+db.routers.create_index("created")
 db.routers.create_index("last_contact")
 db.routers.create_index("netifs.mac")
 db.routers.create_index([("position", "2dsphere")])
