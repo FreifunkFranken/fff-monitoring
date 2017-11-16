992b1b981a3d271fb8fe95bd3f62322ff7195489
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-11-16 13:08:59 +0100

    Deleting stats should be done by separate function once per day
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/web/calcglobalstats.py
+++ ffmap/web/calcglobalstats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Execute every 5 min, 2 mins after alfred comes in (sleep 120 in cron)
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
@@ -9,12 +11,17 @@ from ffmap.maptools import *
 from ffmap.mysqltools import FreifunkMySQL
 from ffmap.stattools import record_global_stats, record_hood_stats
 
+import time
+start_time = time.time()
+
 mysql = FreifunkMySQL()
 detect_offline_routers(mysql)
 detect_orphaned_routers(mysql)
 delete_orphaned_routers(mysql)
-delete_old_stats(mysql)
+#delete_old_stats(mysql) # Only execute once daily, takes 2 minutes
 record_global_stats(mysql)
 record_hood_stats(mysql)
 update_mapnik_csv(mysql)
 mysql.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
