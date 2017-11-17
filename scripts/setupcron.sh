279b7c82c8fe404978c25be76cc4ac9276c115be
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-11-18 00:46:30 +0100

    scripts: Run statistics cron jobs every 5+3 minutes
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- scripts/setupcron.sh
+++ scripts/setupcron.sh
@@ -7,8 +7,8 @@ if crontab -l | grep -q "$monpath" ; then
 	exit 1
 fi
 
-# Runs every 5 min and waits 2 min
-(crontab -l 2>/dev/null; echo "2-59/5 * * * * $monpath/scripts/calcglobalstats.py") | crontab -
+# Runs every 5 min and waits 3 min
+(crontab -l 2>/dev/null; echo "3-59/5 * * * * $monpath/scripts/calcglobalstats.py") | crontab -
 
 # Runs at 4:02
 (crontab -l 2>/dev/null; echo "2 4 * * * $monpath/scripts/deletestats.py") | crontab -
