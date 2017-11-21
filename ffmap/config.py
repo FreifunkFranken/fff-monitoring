362bafde18b86aaedf6e6119903aecb475ef16e9
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-11-21 11:42:29 +0100

    config: Router stats are stored for 14 days instead of 7
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/config.py
+++ ffmap/config.py
@@ -7,7 +7,7 @@ CONFIG = {
 	"offline_threshold_minutes": 15,
 	"orphan_threshold_days": 7,
 	"delete_threshold_days": 180,
-	"router_stat_days": 7,
+	"router_stat_days": 14,
 	"event_num_entries": 20,
 	"global_stat_days": 30,
 	"csv_dir": "/var/lib/ffmap/csv",
