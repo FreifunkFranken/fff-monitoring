d8bc889dbb18574bd8383e7ea1b8c01c55dd30a8
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-11-23 13:26:47 +0100

    user.html: Annotate router if reset happened
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/web/application.py
+++ ffmap/web/application.py
@@ -222,7 +222,7 @@ def user_info(nickname):
 		else:
 			flash("<b>You are not authorized to perform this action!</b>", "danger")
 	routers = mysql.fetchall("""
-		SELECT id, hostname, status, hood, firmware, hardware, created, sys_uptime, clients
+		SELECT id, hostname, status, hood, firmware, hardware, created, sys_uptime, clients, reset
 		FROM router
 		WHERE contact = %s
 		ORDER BY hostname ASC
