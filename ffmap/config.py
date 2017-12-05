20eedde52eb05807e3c599fb97b765414dedc0b5
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-12-05 13:33:15 +0100

    config.py: Add explanation for options
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/config.py
+++ ffmap/config.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 CONFIG = {
-	"vpn_netif": "fffVPN",
-	"vpn_netif_l2tp": "l2tp",
-	"vpn_netif_aux": "fffauxVPN",
-	"offline_threshold_minutes": 15,
-	"orphan_threshold_days": 7,
-	"delete_threshold_days": 180,
-	"router_stat_days": 14,
-	"router_stat_mindiff_secs": 10,
-	"event_num_entries": 20,
-	"global_stat_days": 30,
-	"csv_dir": "/var/lib/ffmap/csv",
-	"debug_dir": "/data/fff",
+	"vpn_netif": "fffVPN",				# Name of VPN interface
+	"vpn_netif_l2tp": "l2tp",			# Beginning of names of L2TP interfaces
+	"vpn_netif_aux": "fffauxVPN",		# Name of AUX interface
+	"offline_threshold_minutes": 15,	# Router switches to offline after X minutes
+	"orphan_threshold_days": 7,			# Router switches to orphaned state after X days
+	"delete_threshold_days": 180,		# Router is deleted after X days
+	"router_stat_days": 14,				# Router stats are collected for X days (if online)
+	"router_stat_mindiff_secs": 10,		# Time difference (uptime) required for a new entry in router stats
+	"event_num_entries": 20,			# Number of events stored per router
+	"global_stat_days": 30,				# Global/hood stats are collected for X days
+	"csv_dir": "/var/lib/ffmap/csv",	# Directory where the .csv files for TileStache/mapnik are stored
+	"debug_dir": "/data/fff",			# Output directory for debug .txt files
 }
