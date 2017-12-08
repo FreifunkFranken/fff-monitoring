b4e5f7594a5eabd1e3a46f8fea6acce2e0d8b7ec
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-12-08 13:35:03 +0100

    scripts: Add crontiles.sh
    Provide regular restart of uwsgi-tiles as a workaround for
    missing dots.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ scripts/crontiles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+command="systemctl restart uwsgi-tiles"
+
+if crontab -l | grep -q "$command" ; then
+	echo "Cron already set."
+	exit 1
+fi
+
+# Runs at X:14
+(crontab -l 2>/dev/null; echo "14 * * * * $command") | crontab -
+
+echo "Cron set successfully."
+exit 0
+
