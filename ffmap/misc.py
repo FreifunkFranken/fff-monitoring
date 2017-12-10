010f47dbe75d2a1976df7374f754319c15c00401
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-12-10 19:04:31 +0100

    routertools: Use custom file for full log, do not write to syslog
    It is easier to evaluate a specific log which is not crowded with
    messages from other programs
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/misc.py
+++ ffmap/misc.py
@@ -3,9 +3,15 @@
 import time
 import datetime
 
+from ffmap.config import CONFIG
+
 def utcnow():
 	return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
 
 def writelog(path, content):
 	with open(path, "a") as csv:
+		csv.write(time.strftime('{%Y-%m-%d %H:%M:%S}') + " - " + content + "\n")
+
+def writefulllog(content):
+	with open(CONFIG["debug_dir"] + "/fulllog.log", "a") as csv:
 		csv.write(time.strftime('{%Y-%m-%d %H:%M:%S}') + " - " + content + "\n")
--- ffmap/routertools.py
+++ ffmap/routertools.py
@@ -165,7 +165,7 @@ def import_nodewatcher_xml(mysql, mac, xml):
 		
 	except ValueError as e:
 		import traceback
-		print("Warning: Unable to parse xml from %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
+		writefulllog("Warning: Unable to parse xml from %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
 		if router_id:
 			set_status(mysql,router_id,"unknown")
 		status = "unknown"
@@ -173,18 +173,18 @@ def import_nodewatcher_xml(mysql, mac, xml):
 		writelog(CONFIG["debug_dir"] + "/fail_readrouter.txt", "XML parsing error: {} - {}".format(mac,e))
 	except OverflowError as e:
 		import traceback
-		print("Warning: Overflow Error when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
+		writefulllog("Warning: Overflow Error when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
 		if router_id:
 			set_status(mysql,router_id,"unknown")
 		status = "unknown"
 		status_comment = "Integer Overflow"
 	except my.OperationalError as e:
 		import traceback
-		print("Warning: Operational error in MySQL when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
+		writefulllog("Warning: Operational error in MySQL when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
 		writelog(CONFIG["debug_dir"] + "/fail_readrouter.txt", "MySQL Error: {} - {}".format(router_update["hostname"],e))
 	except Exception as e:
 		import traceback
-		print("Warning: Exception occurred when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
+		writefulllog("Warning: Exception occurred when saving %s: %s\n__%s" % (mac, e, traceback.format_exc().replace("\n", "\n__")))
 		if router_id:
 			set_status(mysql,router_id,"unknown")
 		status = "unknown"
