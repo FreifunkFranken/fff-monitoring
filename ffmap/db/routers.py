b96ca2cb3961ccc9b641e4ce839611b0f0a90eb2
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2017-12-29 15:24:37 +0100

    api/alfred and router.html: Show WiFi data for netifs
    This shows information about WiFi parameters (e.g. channel).
    
    Except the Tx-Power, data is only available if a firmware with
    a corresponding nodewatcher update is present (version 44).
    
    This requires a change of the MySQL table router_netif!
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/db/routers.py
+++ ffmap/db/routers.py
@@ -146,7 +146,12 @@ mysql.execute("""
 		`tx_bytes` bigint(20) UNSIGNED NOT NULL,
 		`fe80_addr` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
 		`ipv4_addr` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`mac` char(17) COLLATE utf8_unicode_ci NOT NULL
+		`mac` char(17) COLLATE utf8_unicode_ci NOT NULL,
+		`wlan_channel` tinyint(3) UNSIGNED DEFAULT NULL,
+		`wlan_type` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+		`wlan_width` tinyint(3) UNSIGNED DEFAULT NULL,
+		`wlan_ssid` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+		`wlan_txpower` varchar(8) COLLATE utf8_unicode_ci DEFAULT NULL
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 """)
 
