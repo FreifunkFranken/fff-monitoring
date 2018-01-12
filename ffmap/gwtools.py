9e7fcb25639d445963ddc28b5982b1e8789de83c
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2018-01-12 23:19:17 +0100

    api/gwinfo: Suppress interfaces with l2tp*, br-* and short MAC
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/gwtools.py
+++ ffmap/gwtools.py
@@ -30,6 +30,10 @@ def import_gw_data(mysql, gw_data):
 		
 		ndata = []
 		for n in gw_data["netifs"]:
+			if len(n["mac"])<17:
+				continue
+			if n["netif"].startswith("l2tp") or n["netif"].startswith("br-"):
+				continue
 			if not "vpnif" in n or not n["vpnif"]:
 				n["vpnif"] = None
 			ndata.append((newid,n["mac"],n["netif"],n["vpnif"]))
