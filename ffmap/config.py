5249b5e24f61c7f8916f6648c89a066c07bd6294
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2018-04-06 11:32:11 +0200

    config.py: Adjustments for 8 GiB RAM
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/config.py
+++ ffmap/config.py
@@ -4,13 +4,13 @@ CONFIG = {
 	"vpn_netif": "fffVPN",				# Name of VPN interface
 	"vpn_netif_l2tp": "l2tp",			# Beginning of names of L2TP interfaces
 	"vpn_netif_aux": "fffauxVPN",		# Name of AUX interface
-	"offline_threshold_minutes": 10,	# Router switches to offline after X minutes
+	"offline_threshold_minutes": 15,	# Router switches to offline after X minutes
 	"orphan_threshold_days": 7,			# Router switches to orphaned state after X days
 	"delete_threshold_days": 180,		# Router is deleted after X days
 	"gw_netif_threshold_hours": 48,		# Hours which outdated netif from gwinfo is preserved for statistics
 	"router_stat_days": 30,				# Router stats are collected for X days
-	"router_stat_netif": 21,			# Router stats for netifs are collected for X days
-	"router_stat_gw": 7,				# Router stats for gw are collected for X days
+	"router_stat_netif": 7,			# Router stats for netifs are collected for X days
+	"router_stat_gw": 1,				# Router stats for gw are collected for X days
 	"router_stat_mindiff_secs": 10,		# Time difference (uptime) in seconds required for a new entry in router stats
 	"router_stat_mindiff_default": 270,	# Time difference (router stats tables) in seconds required for a new entry in router stats
 	"router_stat_mindiff_netif": 570,	# Time difference (router netif stats) in seconds required for a new entry in router stats
