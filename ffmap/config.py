9a7c4a1eb69adca33b5a4b889a1a22acdd8576a8
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2018-07-03 13:18:41 +0200

    config.py: Adjust netif stats collection (14 days every 5 min)
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- ffmap/config.py
+++ ffmap/config.py
@@ -9,11 +9,11 @@ CONFIG = {
 	"delete_threshold_days": 180,		# Router is deleted after X days
 	"gw_netif_threshold_hours": 48,		# Hours which outdated netif from gwinfo is preserved for statistics
 	"router_stat_days": 30,				# Router stats are collected for X days
-	"router_stat_netif": 21,			# Router stats for netifs are collected for X days
+	"router_stat_netif": 14,			# Router stats for netifs are collected for X days
 	"router_stat_gw": 7,				# Router stats for gw are collected for X days
 	"router_stat_mindiff_secs": 10,		# Time difference (uptime) in seconds required for a new entry in router stats
 	"router_stat_mindiff_default": 270,	# Time difference (router stats tables) in seconds required for a new entry in router stats
-	"router_stat_mindiff_netif": 570,	# Time difference (router netif stats) in seconds required for a new entry in router stats
+	"router_stat_mindiff_netif": 270,	# Time difference (router netif stats) in seconds required for a new entry in router stats
 	"event_num_entries": 300,			# Number of events stored per router
 	"global_stat_days": 365,			# Global/hood stats are collected for X days
 	"csv_dir": "/var/lib/ffmap/csv",	# Directory where the .csv files for TileStache/mapnik are stored
